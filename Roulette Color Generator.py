d4986927b98746ca8c242627f7256b133ffc3793
Alexis-Yovo <74179398+Alexis-Yovo@users.noreply.github.com>
2024-01-29 18:05:54 +0100

    Add files via upload

---
+++ Roulette Color Generator.py
@@ -0,0 +1,12 @@
+import random
+
+colors_list = ["Vert","Roug","Noir"]
+color_choice = input("Entrez le nom d'une couleur: ")
+
+color_random = random.choice(colors_list)
+print(color_random)
+
+if(color_choice == color_random):
+  print("Vous avez pris la bonne couleur.")
+else:
+  print("Vous avez pris la mauvaise couleur.")
